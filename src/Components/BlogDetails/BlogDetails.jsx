2fc9992daf9baa395ddd4539ace157a29c447371
RiyaadHossain <riyadhossain.dev@gmail.com>
2022-04-03 16:06:00 +0600

    Spinner added

--- src/Components/BlogDetails/BlogDetails.jsx
+++ src/Components/BlogDetails/BlogDetails.jsx
@@ -1,36 +1,48 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
-import { BsChevronLeft } from 'react-icons/bs';
+import { BsChevronLeft } from "react-icons/bs";
 import "./BlogDetails.css";
+import Spinner from "../Spinner/Spinner";
 
 const BlogDetails = () => {
-    const Navigate = useNavigate()
+  const Navigate = useNavigate();
   const { id } = useParams();
   const [blog, setBlog] = useState({});
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
+    setLoading(true);
     fetch(`https://retro-tech-talks.herokuapp.com/getBlog/${id}`)
       .then((res) => res.json())
-      .then((data) => setBlog(data));
+      .then((data) => {
+        setBlog(data);
+        setLoading(false);
+      });
   }, [id]);
 
   return (
-    <>
-      <div className="header-gradient" />
-      <div>
-        <button className="back-button" onClick={() => Navigate('/')}>
-          <BsChevronLeft/>
-          <p>Back</p>
-        </button>
-        <div className="blog-details">
-          <div className="blog-image">
-            <img src={blog?.imageURL} alt="" />
+    <div>
+      {loading ? (
+        <Spinner />
+      ) : (
+        <>
+          <div className="header-gradient" />
+          <div>
+            <button className="back-button" onClick={() => Navigate("/")}>
+              <BsChevronLeft />
+              <p>Back</p>
+            </button>
+            <div className="blog-details">
+              <div className="blog-image">
+                <img src={blog?.imageURL} alt="" />
+              </div>
+              <h1>{blog?.title}</h1>
+              <p>{blog?.blog}</p>
+            </div>
           </div>
-          <h1>{blog?.title}</h1>
-          <p>{blog?.blog}</p>
-        </div>
-      </div>
-    </>
+        </>
+      )}
+    </div>
   );
 };
 
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,20 +1,28 @@
 import React, { useEffect, useState } from "react";
 import Blog from "../Blog/Blog";
+import Spinner from "../Spinner/Spinner";
 import './Home.css'
 
 const Home = () => {
   const [blogs, setBlogs] = useState([]);
+  const [loading, setLoading] = useState(false);
   useEffect(() => {
+    setLoading(true)
     fetch("https://retro-tech-talks.herokuapp.com/blogs")
       .then((res) => res.json())
-      .then((data) => setBlogs(data));
+      .then((data) => {
+        setBlogs(data)
+        setLoading(false);
+      });
   }, []);
   return (
-    <div className='blogs-container'>
+    <>
+    {loading ? <Spinner/> : <div className='blogs-container'>
       {blogs.map((blog, index) => (
         <Blog key={index} blog={blog} />
       ))}
-    </div>
+    </div>}
+    </>
   );
 };
 
